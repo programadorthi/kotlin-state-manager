926b3709534dddf055e63a14a284a750991efcca
Thiago Santos <programadorthi@gmail.com>
2024-02-16 15:07:54 -0300

    chore: ios sample app

---
+++ samples/iosSample/iosSample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iosSample
+//
+//  Created by Thiago dos Santos on 16/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ samples/iosSample/iosSample/iosSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosSampleApp.swift
+//  iosSample
+//
+//  Created by Thiago dos Santos on 16/02/24.
+//
+
+import SwiftUI
+
+@main
+struct iosSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
